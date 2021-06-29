30d22c5012b9e03648f43e46ae2828e4d46c971b
abhishek <avishekv444@gmail.com>
2021-06-29 14:53:38 +0530

    inproved deleting functionality

--- controllers/addOrRemove.js
+++ controllers/addOrRemove.js
@@ -16,28 +16,28 @@ module.exports.add=function(req,res){
 };
 
 module.exports.remove=function(req,res){
-    // console.log(req.body)
-    // Object.keys(req.body).forEach(function(key) {
-    //     console.log(key);
-    //     TodoList.findByIdAndDelete(key, function(err) {
+    console.log(req.body)
+    Object.keys(req.body).forEach(function(key) {
+        console.log(key);
+        TodoList.findByIdAndDelete(key, function(err) {
             
-    //         if (err) {
-    //             console.log('Error in deleting an item from database', err);
-    //             return;
-    //         }
-    //         console.log('item is deleted');
-    //     });
-    // });
+            if (err) {
+                console.log('Error in deleting an item from database', err);
+                return;
+            }
+            console.log('item is deleted');
+        });
+    });
   
-    //  res.redirect('/');
-    console.log(req.body);
-    let id = req.body.selectedTodos;
-     console.log(id);
-    TodoList.deleteMany({_id:id}, function(err) {
-        if (err) {
-          console.log("Error in deleting from db");
-        } 
-        return res.redirect('back');
-      });
+    res.redirect('/');
+    //console.log(req.body);
+    // let id = req.body.selectedTodos;
+    //  console.log(id);
+    // TodoList.deleteMany({_id:id}, function(err) {
+    //     if (err) {
+    //       console.log("Error in deleting from db");
+    //     } 
+    //     return res.redirect('back');
+    //   });
     
 };
